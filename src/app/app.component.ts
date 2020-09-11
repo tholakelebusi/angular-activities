0eeb2c67c9302a7c72eee8427486860a2d6f3c69
tholakelebusi <tholakelebusi@gmail.com>
2020-09-11 01:44:48 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, VERSION } from '@angular/core';
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
-})
+})tt
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 }
